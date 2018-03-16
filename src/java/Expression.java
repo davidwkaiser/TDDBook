a3b5cda0d5c5d5bf24a3c30e887aaef4bc4ced3a
davidwkaiser <davidwkaiser@yahoo.com>
2018-03-15 22:33:58 -0500

    Complete Chapter 16 and finish the book!

--- src/java/Expression.java
+++ src/java/Expression.java
@@ -1,5 +1,8 @@
 public interface Expression {
     Money reduce(Bank bank, String to);
 
-    Expression plus(Expression addend);
+    public Expression plus(Expression addend);
+
+    public Expression times(int multiplier);
+
 }
